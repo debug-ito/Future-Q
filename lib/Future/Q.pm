d93d8e712f99f097f5150c5bad71a8da2e0a157b
Toshio Ito <debug.ito@gmail.com>
2014-03-23 17:44:50 +0900

    Release 0.060

--- lib/Future/Q.pm
+++ lib/Future/Q.pm
@@ -8,6 +8,8 @@ use Scalar::Util qw(refaddr blessed weaken);
 use Carp;
 use Try::Tiny ();
 
+our $VERSION = '0.060';
+
 our @CARP_NOT = qw(Try::Tiny Future);
 
 ## ** lexical attributes to avoid collision of names.
@@ -197,8 +199,6 @@ foreach my $method (qw(wait_all wait_any needs_all needs_any)) {
     };
 }
 
-our $VERSION = '0.050';
-
 1;
 
 __END__
@@ -206,10 +206,6 @@ __END__
 =head1 NAME
 
 Future::Q - a Future (or Promise or Deferred) like Q module for JavaScript
-
-=head1 VERSION
-
-Version 0.050
 
 =head1 SYNOPSIS
 
